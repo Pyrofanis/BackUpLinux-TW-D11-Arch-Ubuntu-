8201f2ad3b315ee57950c6392bfd3e8f1bdcdf61
Athanasios Nektarios Karachalios Stagkas <41343849+Pyrofanis@users.noreply.github.com>
2022-04-28 19:42:37 +0300

    Leap and flatpak organization

---
+++ OS Specific/BackupAppSUSSE/Leap/zypper.sh
@@ -0,0 +1,9 @@
+sudo zypper ref
+sudo zypper up
+sudo zypper dup --from packman --allow-vendor-change
+sudo zypper in  -t pattern devel_basis
+sudo zypper in dotnet-sdk-6.0  dotnet-sdk-6.0 mono-complete flatpak  curl openssl mpv aria2 vlc diffutils   git   unityhub  rpmdevtools papirus-icon-theme fetchmsttfonts kernel-source  kernel-syms fish neofetch
+sudo zypper in    -t pattern yast2_server
+sudo zypper dup    --from packman --allow-vendor-change
+sudo zypper install   opi 
+opi  codecs 
--- OS Specific/BackupAppSUSSE/TW/main.sh
+++ OS Specific/BackupAppSUSSE/TW/main.sh
@@ -1,5 +1,4 @@
 sudo zypper --gpg-auto-import-keys refresh
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 wget https://packages.microsoft.com/config/opensuse/15/prod.repo
 sudo mv prod.repo /etc/zypp/repos.d/microsoft-prod.repo
